545c797c9b574afde73a6dd520afb715540f0036
chlloyd <christopher.lloyd3@mail.bcu.ac.uk>
2020-02-03 18:14:49 +0000

    Added adding to path in motionbuilder to find mbtools

--- mirrormotiondata.py
+++ mirrormotiondata.py
@@ -1,6 +1,13 @@
+import sys
+import os
 import pyfbsdk
-import mbtools as mc
+try:
+    import mbtools as mb
+except ImportError:
+    MBTOOLS_PATH = os.path.dirname(os.path.realpath(__file__))
+    if MBTOOLS_PATH not in sys.path:
+        sys.path.append(MBTOOLS_PATH)
+        import mbtools as mb
 
-reload(mc)
-
+reload(mb)
 
