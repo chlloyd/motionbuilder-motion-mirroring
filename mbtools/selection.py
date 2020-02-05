c540a4606188586be7436c3e2a8f07095d8df90b
chlloyd <christopher.lloyd3@mail.bcu.ac.uk>
2020-02-05 22:06:26 +0000

    Added Selection by type function

--- mbtools/selection.py
+++ mbtools/selection.py
@@ -3,11 +3,34 @@ selection.py
 Utility functions for working with component selection.
 """
 
-import pyfbsdk
+from pyfbsdk import FBSystem
 
 
-def list_all():
+def list_all_selected():
+    """Lists all components within the are that are selected.
+
+    Returns:
+        List: list of all components in the scene that are currently selected.
+
     """
-    Returns a list of all components in the scene that are currently selected.
+    return [component for component in FBSystem().Scene.Components if component.Selected]
+
+
+def list_all_by_type(object_type):
+    """Lists all components in the scene by a type.
+
+    Args:
+        object_type (str): Type of object to select.
+
+    Returns:
+        List: All object in the scene by the type object_type.
+
     """
-    return [component for component in pyfbsdk.FBSystem().Scene.Components if component.Selected]
+    # Get all nodes in scene and find joints
+    allNodes = FBSystem().Scene.Components
+    object_list = []
+    for lComponent in allNodes:
+        # print lComponent.Name
+        if lComponent and lComponent.ClassName() == object_type:
+            object_list.append(lComponent)
+    return object_list
