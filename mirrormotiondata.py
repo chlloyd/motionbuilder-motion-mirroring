54de25de1a58dff589febc9ff9e17b71e10c72bc
chlloyd <christopher.lloyd3@mail.bcu.ac.uk>
2020-02-05 21:22:03 +0000

    Added for loop to start mirroring joints

--- mirrormotiondata.py
+++ mirrormotiondata.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pyfbsdk
+
 try:
     import mbtools as mb
 except ImportError:
@@ -9,7 +10,31 @@ except ImportError:
         sys.path.append(MBTOOLS_PATH)
         import mbtools as mb
 
-reload(mb)  # only here for when the library is be edited
+        reload(mb)  # only here for when the library is be edited
+
 
 # TODO: Add Comments on each line
 
+
+def BuildUI():
+    pass
+
+
+def mirror_keyframes():
+    """FOR frames in frame sequence:
+        for rotation in joint:
+            flip from hip joint (change_orientation, rotation)
+            END FOR
+        END FOR"""
+    for frame in range(mb.time.first_frame(), mb.time.last_frame() + 1):
+        pass
+
+
+mirror_keyframes()
+
+"""FUNC flip from hip joint (orientation, rotation):
+    convert quaternion to rotation matrix
+    mirror matrix on orientation
+    convert rotation matrix to quaternion
+    return new quaternion
+    END FUNC"""
