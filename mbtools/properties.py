8f8812a555a7177fda29873870d7cd197067550a
chlloyd <christopher.lloyd3@mail.bcu.ac.uk>
2020-02-10 14:49:36 +0000

    Added properties.py to mbtools

---
+++ mbtools/properties.py
@@ -0,0 +1,18 @@
+"""
+properties.py
+Utility functions for working with component properties.
+"""
+
+from pyfbsdk import FBSystem
+
+
+def set_rotation():
+    pass
+
+
+def set_postion():
+    pass
+
+
+def set_scale():
+    pass
