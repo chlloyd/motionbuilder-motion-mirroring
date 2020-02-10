e5356ad56d6115f8ca36c1321e945fabbee57a4e
chlloyd <christopher.lloyd3@mail.bcu.ac.uk>
2020-02-10 16:53:44 +0000

    Added set translation, rotation and scale

--- mbtools/__init__.py
+++ mbtools/__init__.py
@@ -4,4 +4,4 @@ A module containing functions for Autodesk Motionbuilder.
 These tools are specifically used for manipulating Motion Capture Data within Motionbuilder.
 """
 
-from mbtools import keyframe, keyframes, selection, time
+from mbtools import keyframe, keyframes, selection, time, properties
--- mbtools/properties.py
+++ mbtools/properties.py
@@ -3,16 +3,79 @@ properties.py
 Utility functions for working with component properties.
 """
 
-from pyfbsdk import FBSystem
+from pyfbsdk import FBVector3d
 
 
-def set_rotation():
-    pass
+def get_rotation(object):
+    """Gets the rotation property from an object.
+
+    Args:
+        object: An object in the scene.
+
+    Returns: The rotation of an object.
+
+    """
+    return object.Rotation
 
 
-def set_postion():
-    pass
+def get_position(object):
+    """Gets the position property from an object.
+
+    Args:
+        object: An object in the scene.
+
+    Returns: The position of an object.
+
+    """
+    return object.Translation
 
 
-def set_scale():
-    pass
+def get_scale(object):
+    """Gets the scale property from an object.
+
+    Args:
+        object: An object in the scene.
+
+    Returns: The scale of an object.
+
+    """
+    return object.Scaling
+
+
+def set_rotation(object, rotation):
+    """Sets the rotation property on an object.
+
+    Args:
+        object: An object in the scene.
+        rotation: (Tuple): (x, y, z) rotation data.
+
+    Returns: None
+
+    """
+    object.Rotation = FBVector3d(rotation)
+
+
+def set_position(object, position):
+    """Sets the position property on an object.
+
+    Args:
+        object: An object in the scene.
+        position: (Tuple): (x, y, z) position data.
+
+    Returns: None
+
+    """
+    object.Rotation = FBVector3d(position)
+
+
+def set_scale(object, scale):
+    """Sets the scale property on an object.
+
+    Args:
+        object: An object in the scene.
+        scale: (Tuple): (x, y, z) scale data.
+
+    Returns: None
+
+    """
+    object.Rotation = FBVector3d(scale)
